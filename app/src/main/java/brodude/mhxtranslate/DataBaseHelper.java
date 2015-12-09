c2e81534d195c4081bf3371d6ef9bf3d97d2103f
djs6255 <djs6255@gmail.com>
2015-12-08 23:38:07 -0800

    Simple lists with lising by character length.

--- app/src/main/java/brodude/mhxtranslate/DataBaseHelper.java
+++ app/src/main/java/brodude/mhxtranslate/DataBaseHelper.java
@@ -6,6 +6,7 @@ package brodude.mhxtranslate;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.annotation.NonNull;
@@ -16,6 +17,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
@@ -23,7 +25,7 @@ import java.util.ListIterator;
 public class DataBaseHelper extends SQLiteOpenHelper {
     private static final int DATABASE_VERSION = 1;
 
-    private static final String DATABASE_NAME = "mhxSkillMap";
+    private static final String DATABASE_NAME = "mhxSkillMap.db";
 
     private static final String TABLE_SKILLMAP = "jpengskll";
 
@@ -37,9 +39,10 @@ public class DataBaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_SKILLMAP);
         String CREATE_SKILLS_TABLE = "CREATE TABLE " + TABLE_SKILLMAP + "("
-                + COL_ID + " INTEGER PRIMARY KEY, " +  COL_JP + " TEXT,"
-                + COL_ENG + " TEXT" + ")";
+                + COL_ID + " INTEGER PRIMARY KEY, " +  COL_JP + " TEXT NOT NULL UNIQUE,"
+                + COL_ENG + " TEXT NOT NULL UNIQUE" + ")";
         db.execSQL(CREATE_SKILLS_TABLE);
     }
 
@@ -49,7 +52,7 @@ public class DataBaseHelper extends SQLiteOpenHelper {
         onCreate(db);
     }
 
-    public void addSkill(String jpText, String engText) {
+    public void addSkill(String engText, String jpText) {
         SQLiteDatabase db = this.getWritableDatabase();
 
         ContentValues values = new ContentValues();
@@ -59,4 +62,19 @@ public class DataBaseHelper extends SQLiteOpenHelper {
         db.close();
     }
 
+    public HashMap<String, String> getSkillsByLength(String length){
+        HashMap<String, String> skillList = new HashMap<String, String>();
+
+        String lengthQuery = "SELECT " + COL_JP + ", " + COL_ENG + " FROM "
+                + TABLE_SKILLMAP + " WHERE LENGTH(" + COL_JP + ") = " + length + ";";
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(lengthQuery, null);
+
+        if(cursor.moveToFirst()) {
+            do {
+                skillList.put(cursor.getString(0), cursor.getString(1));
+            } while (cursor.moveToNext());
+        }
+        return skillList;
+    }
 }
--- app/src/main/java/brodude/mhxtranslate/DisplayCharList.java
+++ app/src/main/java/brodude/mhxtranslate/DisplayCharList.java
@@ -8,8 +8,12 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class DisplayCharList extends AppCompatActivity {
 
@@ -17,15 +21,21 @@ public class DisplayCharList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Intent intent = getIntent();
-        String chars = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-        String[] skillList = intent.getStringArrayExtra(MainActivity.VALUE_LIST);
-        for (String skill : skillList) {
-            chars += "\n " + skill;
+        String chars = intent.getStringExtra(MainActivity.CHAR_LENGTH) + " Character skills";
+        HashMap<String, String> skillList = (HashMap<String, String>)intent.getSerializableExtra(MainActivity.SKILLS_HASH);
+        if (skillList.isEmpty()) {
+            chars += "\nNo skills found!";
+        }
+        else {
+            for (Map.Entry<String, String> entry : skillList.entrySet()) {
+                chars += "\n " + entry.getKey() + " - " + entry.getValue();
+            }
         }
         //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         TextView textView = new TextView(this);
-        textView.setTextSize(40);
+        textView.setTextSize(22);
         textView.setText(chars);
+        textView.setMovementMethod(new ScrollingMovementMethod());
         setContentView(textView);
 
 
